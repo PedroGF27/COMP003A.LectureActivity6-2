91a51b3a5f32daca28bccd365712284ed1aaa3c3
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-19 13:47:12 -0800

    added shapecalculator class

---
+++ COMP003A.LectureActivity6-2/ShapeCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_2
+{
+    class ShapeCalculator
+    {
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public double CalculateArea(double baseLength, double height, bool isTriangle)
+        {
+            return 0.5 * baseLength * height;
+        }
+    }
+}
