573c74158b2add708b2982b30735bb39ea333f92
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-19 13:53:57 -0800

    Added circle class

---
+++ COMP003A.LectureActivity6-2/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_2
+{
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius) 
+        {
+            Radius = radius;
+        }
+
+        public override void DisplayArea()
+        {
+            Console.WriteLine($"Circle Area: {Math.PI * Radius * Radius}");
+        }
+    }
+}
