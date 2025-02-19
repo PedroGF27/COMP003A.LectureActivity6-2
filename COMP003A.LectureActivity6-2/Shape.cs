e2f952008ff3922fd83e6b70cf2c9d3a711b2e68
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-19 13:49:11 -0800

    added shape class

---
+++ COMP003A.LectureActivity6-2/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_2
+{
+    internal class Shape
+    {
+        public virtual void DisplayArea()
+        {
+            Console.WriteLine("Calculating area of a shape.");
+        }
+    }
+}
