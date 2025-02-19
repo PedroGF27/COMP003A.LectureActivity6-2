25e66872679f15a4584a197e78e4f00524f55977
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-19 14:08:02 -0800

    Implemented classes into main

--- COMP003A.LectureActivity6-2/Program.cs
+++ COMP003A.LectureActivity6-2/Program.cs
@@ -8,7 +8,17 @@ namespace COMP003A.LectureActivity6_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShapeCalculator calculator = new ShapeCalculator();
+            Console.WriteLine($"Circle Area: {calculator.CalculateArea(5)}");
+            Console.WriteLine($"Rectangle Area {calculator.CalculateArea(5, 10)}");
+            Console.WriteLine($"Triangle Area: {calculator.CalculateArea(5, 10, true)}");
+
+            Shape myCircle = new Circle(7);
+
+            Shape myRectangle = new Rectangle(4, 8);
+
+            myCircle.DisplayArea();
+            myRectangle.DisplayArea();
         }
     }
 }
